73440b0392e1887618f03b99a11bf75f1363fb8d
Stephane <anonim@ous.com>
2022-08-01 20:30:57 +0200

    refactoring

--- src/main/java/com/adeso/TextAreaRightController.java
+++ src/main/java/com/adeso/TextAreaRightController.java
@@ -6,7 +6,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
 
-public class PrimaryController {
+public class TextAreaRightController {
 
     @FXML
     private Button btn1;
